928c92b9c62c23f63624b64273ae4b4dbbe4b328
mashfied <victoreslieh@gmail.com>
2024-09-25 17:52:18 +0300

    passed test

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier = 4){
+    return function(fare){
+        return fare * multiplier;
+    };
+}
+function fareDoubler(fare){
+    const doubler = Math.abs(fare * 2);
+    return doubler;
+}
+function fareTripler(fare){
+    const tripler = Math.abs(fare *3);
+    return tripler;
+}
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
